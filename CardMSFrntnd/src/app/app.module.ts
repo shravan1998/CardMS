ac1f303ee1346c98c40dbb5bbd2beeabfb82ed0e
shravan1998 <shrkum51@gmail.com>
2021-07-06 16:15:48 +0530

    Installed Bootstrap and Generated component

--- CardMSFrntnd/src/app/app.module.ts
+++ CardMSFrntnd/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditCardComponent } from './edit-card/edit-card.component';
+import { ViewCardComponent } from './view-card/view-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditCardComponent,
+    ViewCardComponent
   ],
   imports: [
     BrowserModule,
