1296665a12fb58c7046e26b88b91591f5572fe5d
shravan1998 <shrkum51@gmail.com>
2021-07-06 16:39:48 +0530

    Added dummy frontend

--- CardMSFrntnd/src/app/app-routing.module.ts
+++ CardMSFrntnd/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditCardComponent } from './edit-card/edit-card.component';
+import { ViewCardComponent } from './view-card/view-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',component:EditCardComponent
+},{
+  path:'view',component:ViewCardComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
