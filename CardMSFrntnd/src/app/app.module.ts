eedfd54e1e0c6bcd26b88c683e8fb330c4124246
shravan1998 <shrkum51@gmail.com>
2021-07-07 12:22:13 +0530

    Added Reactive form

--- CardMSFrntnd/src/app/app.module.ts
+++ CardMSFrntnd/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { EditCardComponent } from './edit-card/edit-card.component';
 import { ViewCardComponent } from './view-card/view-card.component';
@@ -14,7 +15,9 @@ import { ViewCardComponent } from './view-card/view-card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- CardMSFrntnd/src/app/edit-card/edit-card.component.ts
+++ CardMSFrntnd/src/app/edit-card/edit-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-card',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-card.component.css']
 })
 export class EditCardComponent implements OnInit {
-
-  constructor() { }
+  cardForm!:FormGroup;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.cardForm = this.fb.group({
+      CardNo:['',[Validators.required]],
+      username:['',[Validators.required]],
+      MM:['',[Validators.required,Validators.min(1),Validators.max(12)]],
+      YY:['',[Validators.required,Validators.min(2021),Validators.max(2030)]]
+    });
+  }
+  onSubmit(){
+
   }
 
 }
